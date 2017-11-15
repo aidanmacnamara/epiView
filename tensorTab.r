c8395ad30b6dc03c92da2dfbc6bf91b312cfb073
David Wille <david.r.wille@gsk.com>
2017-11-15 16:27:37 +0000

    initial version

---
+++ tensorTab.r
@@ -0,0 +1,13 @@
+#' top level wrapper for a tensor tab
+#'
+#' @param input Shiny 'input' list
+#' @param output Shiny 'output' list
+#' @param ui if TRUE defines the UI component, o/w the server
+#'
+#' @return
+#' @export
+#'
+#' @examples
+tensorTab=function(input,output,ui=T){
+  
+}
