2df4e3d8dc154f1a04596b03431239d422bee990
Aidan MacNamara <am673712@uk1salx00235.corpnet2.com>
2017-11-15 07:11:34 +0000

    update

---
+++ global.R
@@ -0,0 +1,8 @@
+
+# global objects
+
+load("dat.RData") # main data object
+load("gene_list_all.RData") # granges object (column annotation)
+load("t_list.RData") # for sushi plot
+load("msig_go_bp.RData") # gene sets for enrichment
+
