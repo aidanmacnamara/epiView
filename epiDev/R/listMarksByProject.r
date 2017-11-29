47346cc604473f71a2fe9820c8abaf4ca7a3e8bc
David Wille <david.r.wille@gsk.com>
2017-11-29 17:58:10 +0000

    updating comments but I think this may actually no be defunct.

--- epiDev/R/listMarksByProject.r
+++ epiDev/R/listMarksByProject.r
@@ -1,18 +1,13 @@
-#' lists availablity of markers by project
+#' DEFUNCT lists availablity of markers by project
+#' 
+#' Think this no longer works - just gives a list of marks ignorning projects
 #'
-#' @param x
+#' @param x a data object returned by Aidan's global_choice()
 #'
-#' @return
+#' @return a charactor vector with the names of the contained marks
 #' @export
 #'
 #' @examples
 listMarksByProject=function(x){
-
-  z <<- x
-
-  print(ls())
   names(x$tmp)
-
-
-
 }
