481a733b8eefb7c45c54190c28417b7c01b2ac99
aidanmacnamara <aidan.macnamara@gmail.com>
2018-06-29 10:47:20 +0100

    update

--- server.R
+++ server.R
@@ -466,7 +466,7 @@ shinyServer(function(input, output, session) {
     for(i in 1:length(data_type)) {
       for(j in sample_ix) {
         # x = str_replace(dat()[[data_type[i]]]$annot$Bigwig[j], "/GWD/bioinfo/projects/", "z:/links/")
-        x = str_replace(dat()[[data_type[i]]]$annot$Bigwig[j], "/GWD/bioinfo/projects/", "http://ftp.ebi.ac.uk/pub/databases/opentargets/")
+        x = str_replace(dat()[[data_type[i]]]$annot$Bigwig[j], "/GWD/bioinfo/projects/RD-Epigenetics-NetworkData/", "http://ftp.ebi.ac.uk/pub/databases/opentargets/")
         if(file.exists(x)) {
           my_tracks_df[[i]] = c(my_tracks_df[[i]], list(as.data.frame(import.bw(x, which=roi))[,c(1:3,6)]))
         } else {
